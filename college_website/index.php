b36ddbc775c4b4e0695bdaad760875831d9eb00c
ivarledgis90@gmail.com <ivarledgis90@gmail.com>
2017-12-04 04:08:19 +0545

    1st commit

---
+++ college_website/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academia International College</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	 <link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <!--  <script type="text/javascript" src="js/image.js"></script> -->
+</head>
+<body>
+
+	<div class="main-container">
+		<div id="header">
+      <div id="notice">
+        
+        Notice:
+
+      </div>
+      <div id="marquee">
+        
+        <marquee>Colz will remain closed until friday!! </marquee>
+      </div>
+
+      <div id="login">
+        
+       <font color="white"> Login</font>
+
+          </div>
+
+	   </div>		
+        <div id="logo">
+  </div>
+
+<div id="nav-bar">
+  
+  <ul>
+    <li class="active"><a href="#">Home</a></li>
+    
+    <li><a href="#">About us</a></li>
+    <li><a href="#">Course</a></li>
+    <li><a href="#">Result</a></li>
+    <li><a href="#">Profile</a></li> 
+    <li class="dropdwn">
+      <a href="#" class="btndwn">Academic</a>
+      <ul class="itmdown">
+        <li><a href="#">Subitem #1</a></li>
+        <li><a href="#">Subitem #2</a></li>
+        <li><a href="#">Subitem #3</a></li>
+        <li><a href="#">Subitem #4</a></li>
+      </ul>
+    </li>
+    <li class="dropdwn">
+      <a href="#" class="btndwn">Notes</a>
+      <ul class="itmdown">
+        <li><a href="#">First sem</a></li>
+        <li><a href="#">Second sem</a></li>
+        <li><a href="#">Third sem</a></li>
+        <li><a href="#">Fourth sem</a></li>
+      </ul>
+    </li>
+    <li class="right"><a href="#">Search</a></li>
+    <li class="right"><a href="#">Submit</a></li>
+  </ul>
+</div>
+
+<div class="slideshow-container">
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/1.jpg" alt="Academia International College">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/2.jpg" alt="Academia International College" style="width: 100%;">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+     <div class="item">
+      <img src="img/3.jpg" alt="Academia International College" style="width: 100%;">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+</div><!-- slideshow-container closed -->
+
+
+<div id="news">
+  <div class="newsfeed">
+      NEWS FEED
+
+   </div>
+   <div class="newsposted">
+     <div class="news1">
+       <img src="img/1.jpg">
+       
+     </div>
+     <div class="news1">
+       <img src="img/1.jpg">
+
+     </div>
+     <div class="news1">
+       <img src="img/1.jpg">
+
+     </div>
+   </div>
+
+</div><!-- News closed --> 
+
+<div class="clearfix main-footer">
+
+  <div id="information">
+<p class="footertitle">INFORMATION:</P>
+We are trying to provide the best home away from home.You will have a homely environment here.. We will try our best to provide 
+you the luxirious day here.
+</div>
+
+  <div id="Contact-us">
+<p class="footertitle">CONTACT US:</P>
+Gwarko Chowk<br/>
+Pilachhen, 
+Lalitpur - 7 <br/>
+P.O.Box: 6246, Kathmandu, Nepal. <br/>
+Tel: 01-6923468, 5525393, 5543037 <br/>
+Email: mail@academiacollege.edu.np <br/>
+</div>
+
+  <div id="Find-us-on">
+<p class="footertitle">FIND US ON:</p>
+    <div id="lowertag">
+  <br><a href="https://www.facebook.com/academiacollege/"  target="_blank"><img src="img/fb1.png"></a>
+  <a href="https://www.gmail.com" target="_blank"><img src="img/gmail.png"></a>
+  <a href="https://www.twitter.com" target="_blank"><img src="image/tw.png"></a>
+    </div>
+
+  </div>
+
+</div><!-- main footer closed -->
+<hr class="line">
+<div class="clearfix copyright">&copy;Academia International College, All Right Reserved</div>
+</div>
+
+</div><!-- main-conatiner div closd -->
+
+
+</body>
+</html>
